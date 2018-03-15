b6cc1a667d588841575c159a74c12b256456eb3d
Rudi Visser <rudi@cedita.com>
2018-03-15 17:33:45 +0000

    eugh

---
+++ Chic/Collection`TEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Chic
+{
+    public class Collection<TEntity> : IQueryable<TEntity>
+        where TEntity : class
+    {
+        public Type ElementType => typeof(TEntity);
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQueryProvider Provider { get; }
+
+        public Collection(IQueryProvider queryProvider)
+        {
+            Provider = queryProvider;
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chic/ConnectedContext.cs
@@ -0,0 +1,11 @@
+﻿using Chic.ChangeTracking;
+using System;
+
+namespace Chic
+{
+    public class ConnectedContext
+    {
+
+        public IChangeTracker changeTracker;
+    }
+}
